697059f0f50416b6f0c1d66c1912f1c3e4b7231b
duytannlu <21130531@st.hcmuaf.edu.vn>
2023-10-30 11:49:39 +0700

    querry List User in UserDao

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -5,6 +5,9 @@ import model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UserDAO {
     private Connection con;
@@ -43,5 +46,21 @@ public class UserDAO {
         }
 
         return user;
+    }
+    public List<User> getAllUser() {
+        String query = "select u.id, u.username, u.email, u.password, u.ip, u.moblie, u.admin";
+        List<User> list = new ArrayList<>();
+        try {
+            Connection con = new DBContext().getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                list.add(new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)
+                                    , rs.getString(5), rs.getString(6), rs.getString(7)));
+
+            }
+        }catch (Exception e){
+        }
+        return list;
     }
 }
